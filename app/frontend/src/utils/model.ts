94af0b2a05008cb18b6cad8323c302e706e89b2d
Rodrigo Baron <baron.rodrigo0@gmail.com>
2024-01-07 23:44:39 -0300

    add frontend models
    Signed-off-by: Rodrigo Baron <baron.rodrigo0@gmail.com>

--- app/frontend/src/utils/model.ts
+++ app/frontend/src/utils/model.ts
@@ -49,6 +49,55 @@ const models: Record<string, ModelCard> = {
     homepage: "https://huggingface.co/meta-llama/Llama-2-13b-chat-hf",
     favicon: "/assets/avatars/llama.png",
   },
+  "llama2-7b": {
+    id: "Llama-2-7b-chat-hf",
+    name: "Llama-2-7b-chat-hf",
+    homepage: "https://huggingface.co/meta-llama/Llama-2-7b-chat-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "codellama-7b": {
+    id: "CodeLlama-7b-Instruct-hf",
+    name: "CodeLlama-7b-Instruct-hf",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "tinyllama-1.1b": {
+    id: "TinyLlama-1.1B-Chat-v1.0",
+    name: "TinyLlama-1.1B-Chat-v1.0",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "openchat-3.5": {
+    id: "openchat-3.5-1210",
+    name: "openchat-3.5-1210",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "codeninja-1.0-openchat-7b": {
+    id: "CodeNinja-1.0-OpenChat-7B",
+    name: "CodeNinja-1.0-OpenChat-7B",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "mistral-7b-v0.2": {
+    id: "Mistral-7B-Instruct-v0.2",
+    name: "Mistral-7B-Instruct-v0.2",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "mixtral-8x7b-offload": {
+    id: "Mixtral-8x7B-Instruct-v0.1-offloading-demo",
+    name: "Mixtral-8x7B-Instruct-v0.1-offloading-demo",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
+  "phi-2": {
+    id: "phi-2",
+    name: "phi-2",
+    homepage: "https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf",
+    favicon: "/assets/avatars/llama.png",
+  },
 };
+
 
 export default models;
